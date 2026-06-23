a4e30234a67cd446140f143a59fa92b5cde80352
guruprasad06 <mguruprasad2006@gmail.com>
2026-06-23 23:16:05 +0530

    backend, schema , bcrypt , jwt tokens

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,6 +12,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+const authRoutes = require("./routes/authRoutes");
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.json({
@@ -20,6 +23,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
