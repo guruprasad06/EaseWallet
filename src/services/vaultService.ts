10d9e604007f1f832b66978eddd515581a0770d7
guruprasad06 <mguruprasad2006@gmail.com>
2026-06-26 15:44:23 +0530

    replaced fake dat to real data

--- src/services/vaultService.ts
+++ src/services/vaultService.ts
@@ -1,7 +1,17 @@
-import { vaultItems } from "../mock/vaultItems";
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/api/vault";
 
 export const vaultService = {
   getItems: async () => {
-    return vaultItems;
+    const token = localStorage.getItem("token");
+
+    const response = await axios.get(API_URL, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return response.data;
   },
 };
