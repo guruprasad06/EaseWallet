c13f7c8d5c07b4c8a301359f8628f359c4b9389c
guruprasad06 <mguruprasad2006@gmail.com>
2026-06-27 20:23:21 +0530

    minor changes

--- backend/src/routes/vaultRoutes.js
+++ backend/src/routes/vaultRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const upload =require("../middleware/uploadMiddleware");
 const protect = require("../middleware/authMiddleware");
 
 const {
@@ -18,5 +18,37 @@ router.get("/", protect, getVaultItems);
 router.delete("/:id", protect, deleteVaultItem);
 
 router.put("/:id", protect, updateVaultItem);
+const VaultItem = require("../models/VaultItem");
+
+router.post(
+  "/upload",
+  protect,
+  upload.single("file"),
+  async (req, res) => {
+    console.log("FILE:", req.file);
+    console.log("BODY:", req.body);
+
+    if (!req.file) {
+      return res.status(400).json({
+        message: "No file received",
+      });
+    }
+
+    try {
+      const item = await VaultItem.create({
+        userId: req.user.id,
+        title: req.file.originalname,
+        type: "document",
+        content: req.file.path,
+      });
+
+      res.status(201).json(item);
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
+    }
+  }
+);
 
 module.exports = router;
